a7861d7e67faa86b59ea1fada6427d6a4e9e8a33
dinhminhhieu <dinhminhhieuvn@gmail.com>
2023-06-01 08:44:47 +0700

    add host new, host sale, best seller product

--- LinyBookStore/Controllers/HomeController.cs
+++ LinyBookStore/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LinyBookStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,15 @@ namespace LinyBookStore.Controllers
 {
     public class HomeController : Controller
     {
+        LinyBookStoreEntities db = new LinyBookStoreEntities();
         public ActionResult Index()
         {
+            ViewBag.AvgFeedback = db.Comments.ToList();
+            ViewBag.HotProduct = db.Products.Where(item => item.status == "1" && item.quantity != "0").OrderByDescending(item => item.buyturn + item.view).Take(8).ToList();
+            ViewBag.NewProduct = db.Products.Where(item => item.status == "1" && item.quantity != "0").OrderByDescending(item => item.create_at).Take(8).ToList();
+            ViewBag.Laptop = db.Products.Where(item => item.status == "1" && item.type == 1 && item.quantity != "0").OrderByDescending(item => item.buyturn + item.view).Take(8).ToList();
+            ViewBag.Accessory = db.Products.Where(item => item.status == "1" && item.type == 2 && item.quantity != "0").OrderByDescending(item => item.buyturn + item.view).Take(8).ToList();
+            ViewBag.OrderDetail = db.OrderDetails.ToList();
             return View();
         }
 
